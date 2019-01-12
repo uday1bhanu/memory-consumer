cf95c35d82cf480a530ac7b25281aa4dfaf4e441
Uday Bhanu Prasad Kanagala <uday.kanagala@anaplan.com>
2019-01-11 22:48:49 -0800

    Update Readme & Fix edge cases

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # memory-consumer
 Sample project to demonstrate memory consumption
+
+* ##### /consume
+    ```
+    curl 'http://localhost:8081/consume' \
+        -H 'Connection: keep-alive' \
+        -H 'Cache-Control: max-age=0' \
+        -H 'Upgrade-Insecure-Requests: 1' \
+        -H 'User-Agent: commandline' \
+        --compressed
+    ```
+    
+* ##### /release
+    ```
+    curl 'http://localhost:8081/release' \
+        -H 'Connection: keep-alive' \
+        -H 'Cache-Control: max-age=0' \
+        -H 'Upgrade-Insecure-Requests: 1' \
+        -H 'User-Agent: commandline' \
+        --compressed
+    ```
--- src/main/java/com/uday/memoryconsumer/controller/MemoryConsumerController.java
+++ src/main/java/com/uday/memoryconsumer/controller/MemoryConsumerController.java
@@ -9,22 +9,29 @@ import java.util.Vector;
 @RestController
 public class MemoryConsumerController {
     Vector v = new Vector();
+    private static final int memorySize = 104857600;
 
     @RequestMapping(value = "/consume", method = RequestMethod.GET)
     public String consume(){
-        byte b[] = new byte[104857600];
+        byte b[] = new byte[memorySize];
         v.add(b);
         Runtime rt = Runtime.getRuntime();
         System.out.println( "free memory: " + rt.freeMemory() );
-        return "Free Memory: " +rt.freeMemory();
+        return "Total Consumed "+ memorySize*v.size() +" | Total Free Memory: " +rt.freeMemory();
     }
 
     @RequestMapping(value = "/release", method = RequestMethod.GET)
     public String release(){
         int size = v.size();
-        v.remove(size-1);
         Runtime rt = Runtime.getRuntime();
-        System.out.println( "free memory: " + rt.freeMemory() );
-        return "Free Memory: " +rt.freeMemory();
+
+        if(size>0) {
+            v.remove(size - 1);
+            System.out.println("free memory: " + rt.freeMemory());
+            return "Total Consumed "+ memorySize*v.size() + " | Total Free Memory: " + rt.freeMemory();
+        }
+        else{
+            return "Nothing to release!";
+        }
     }
 }
